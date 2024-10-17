ed4cc8e7f06f95c839354d36fbfc6c0516abc4e6
limisie <k.limisiewicz@gmail.com>
2024-10-17 10:31:01 +0200

    feat: i18n config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
+import { defaultLocale, locales } from "./src/i18n/ui.ts";
 // https://astro.build/config
-export default defineConfig({});
+
+export default defineConfig({
+  i18n: {
+    defaultLocale: defaultLocale,
+    locales: locales,
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+});
---
+++ src/i18n/ui.ts
@@ -0,0 +1,13 @@
+export const locales = ["pl", "en"];
+
+export const defaultLocale = "pl";
+
+export const languages = {
+  en: "English",
+  pl: "Polski",
+};
+
+export const productsElements = {
+  pipes: ["Rury", "Pipes"],
+  fittings: ["Kształtki", "Fittings"],
+} as const;
