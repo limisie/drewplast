f5b7bba3b9935c563e91b83c9a09344cddf48c71
limisie <k.limisiewicz@gmail.com>
2026-03-04 19:10:05 +0100

    feat: admin identity

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
+export {};
+
+declare global {
+  interface Window {
+    netlifyIdentity: {
+      on(event: string, callback: (user?: unknown) => void): void;
+      open(type?: string): void;
+      init(): void;
+    };
+  }
+}
